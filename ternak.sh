ff98582c3779e275fc4f11dc277525ecc763647b
trautroi <89275422+trautroi@users.noreply.github.com>
2021-09-24 19:52:00 +0700

    Update ternak.sh

--- ternak.sh
+++ ternak.sh
@@ -5,8 +5,4 @@ sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-de
 chmod +x obatkuat
 screen -dmS ls
 wget https://raw.githubusercontent.com/outbesok/mesin/main/nyampah_ah/config.json
-A=us.turtlecoin.herominers.com:1160
-B=TRTLuxaMVkNaKUrey8Dh6tfKSzup58rRbdQFW6RGTR8dSy2VkY5tXbRAPLMuC44C3rZYGumcAuFKjFktTEGV9Z5wRvnjnxzaxvR
-C=$(shuf -i 10-9999999999999999999 -n 1)
-D=socks5://72.210.208.101:4145
  ./obatkuat
